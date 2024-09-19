0c855f8161da32570aeb7d648ff37f5cf8809dfa
Mareamsefan <111729393+Mareamsefan@users.noreply.github.com>
2024-09-19 11:49:08 +0200

    fixed Avatar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import Student from "./components/Student";
 function App() {
   return (
     <>
-  <h1>Start</h1>
   <Student name="ola" id="1"/> 
   </>
   ); 
--- frontend/src/components/Avatar.tsx
+++ frontend/src/components/Avatar.tsx
@@ -2,9 +2,8 @@ type AvatarProps = {
     name: string; 
 
 }
-const Avatar = (props:AvatarProps) => {
+export default function Avatar(props:AvatarProps){
     const {name} = props; 
     const firstLetter = name.split("");      
     return <p>{firstLetter[0].toUpperCase()}</p>
 }
-export default Avatar; 
