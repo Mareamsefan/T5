3c4b3a90ca9d39c7a8c332bf9572164fe5ebc8b8
Mareamsefan <111729393+Mareamsefan@users.noreply.github.com>
2024-09-26 11:55:06 +0200

    gjort ferdig oppgave 14 fra liveoppgave T5.

--- frontend/src/components/Grid.tsx
+++ frontend/src/components/Grid.tsx
@@ -2,25 +2,23 @@ import { useState } from "react";
 import { Student as StudentProps } from "./types";
 import Student from "./Student";
 import AppStudentForm from "./AppStudentForm";
+import Total from "./Total";
 
 type GridProps = {
-    studentList: StudentProps[]
+    studentList: StudentProps[], 
+    onAddStudent: ({name}: {name: string}) => void; 
 }
 
 export default function Grid(props:GridProps) {
-
-    const [studentList, setStudentList] = useState<StudentProps[]>(props.studentList ?? []);
-
+  
+    const {studentList, onAddStudent} = props
     /*
     const onAddStudent = (student:StudentProps, studentList:StudentProps[]) => {
         const studentListCopy = studentList; 
         studentListCopy.push(student)
         setStudentList(studentListCopy)
     }*/ 
-   const onAddStudent =  (student: {name:string}) => {
-    setStudentList((prev) => [...prev, {id: crypto.randomUUID(), ...student}]); 
-   }; 
-    
+ 
     return ( 
     <section className="grid">
         {studentList.map(student => <Student key={student.id} id={student.id} name={student.name}  /> )}
